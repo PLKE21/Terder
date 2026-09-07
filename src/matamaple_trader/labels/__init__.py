35010cdac528186a7e9c8b2778dfa23d6c15a612
Chessman <146985412+PLKE21@users.noreply.github.com>
2026-09-08 05:33:23 +0700

    12 label contracts: export label API

---
+++ src/matamaple_trader/labels/__init__.py
@@ -0,0 +1,3 @@
+from .contracts import LabelContract, TaskType, forward_return_contract, make_labels, n_bar_direction_contract, triple_barrier_contract
+
+__all__ = ["LabelContract", "TaskType", "forward_return_contract", "n_bar_direction_contract", "triple_barrier_contract", "make_labels"]
