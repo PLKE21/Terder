fc1d29b7368909d1724084f4c104f7ba5eadd63d
Chessman <146985412+PLKE21@users.noreply.github.com>
2026-09-09 04:20:31 +0700

    Export dry-run order adapter

--- src/matamaple_trader/execution/__init__.py
+++ src/matamaple_trader/execution/__init__.py
@@ -1,5 +1,14 @@
 from .costs import CostBreakdown, CostConfig, CostEngine, FillResult
 from .guard import ExecutionContext, ExecutionDecision, FBSExecutionGuard
+from .order_adapter import (
+    ExecutionMode,
+    ExecutionReceipt,
+    FBSOrderAdapter,
+    OrderPlan,
+    PreflightResult,
+    normalize_volume,
+    size_volume_from_stop,
+)
 
 __all__ = [
     "CostBreakdown",
@@ -9,4 +18,11 @@ __all__ = [
     "ExecutionContext",
     "ExecutionDecision",
     "FBSExecutionGuard",
+    "ExecutionMode",
+    "ExecutionReceipt",
+    "FBSOrderAdapter",
+    "OrderPlan",
+    "PreflightResult",
+    "normalize_volume",
+    "size_volume_from_stop",
 ]
