7bd54345462c6cb8901af71e9d8eaa91fc40db43
Chessman <146985412+PLKE21@users.noreply.github.com>
2026-09-08 16:09:27 +0700

    23-24 grade: add grading exports

---
+++ src/matamaple_trader/grading/__init__.py
@@ -0,0 +1,7 @@
+from .engine import GradeConfig, GradeInputs, GradeResult, grade_signal
+from .validation import GradeMetrics, GradeTradeResult, validate_grade_performance
+
+__all__ = [
+    "GradeConfig", "GradeInputs", "GradeResult", "grade_signal",
+    "GradeMetrics", "GradeTradeResult", "validate_grade_performance",
+]
